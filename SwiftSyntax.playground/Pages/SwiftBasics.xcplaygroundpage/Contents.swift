31eb511380d1761b8422013dd05b2e04576f92d1
StationAstral <cougarjonez@gmail.com>
2023-10-21 09:50:35 -0700

    # 1

---
+++ SwiftSyntax.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+var greeting = "Hello, playground"
+
+// Print to the console
+// We are going to use the console in Xcode to get feedback from our code
+print(greeting)
+
+
+
+/*
+ This is a
+    multi-line
+        comment
+ */
+
